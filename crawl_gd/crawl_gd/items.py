89440251690758459920c7bd643763dccde0e30d
Ian Thompson <ian.thompson@hrblock.com>
2022-08-06 13:00:41 -0500

    modified:   crawl_gd/spiders/gd_spider.py 	- set start_url to local file path for testing 	- parsing web elements and adding to items for future storage modified:   crawl_gd/items.py 	- using items as way to organize data from each job listing

--- crawl_gd/crawl_gd/items.py
+++ crawl_gd/crawl_gd/items.py
@@ -9,4 +9,25 @@ import scrapy
 class CrawlGdItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # li
+    ID = scrapy.Field()
+    AdvType = scrapy.Field()
+    IsOrganicJob = scrapy.Field()
+    AdOrderID = scrapy.Field()
+    SGOCID = scrapy.Field()
+    IsEasyApply = scrapy.Field()
+    Title = scrapy.Field()
+    Location = scrapy.Field()
+    LocationID = scrapy.Field()
+    LocationType = scrapy.Field()
+
+    # li > div > a
+    HREF = scrapy.Field()
+    Company = scrapy.Field()
+
+    # li > div > a > span > img
+    LogoSRC = scrapy.Field()
+    LogoTitle = scrapy.Field()
+
+    # # li > div > span
+    # CompanyRating = scrapy.Field()
--- crawl_gd/crawl_gd/spiders/gd_spider.py
+++ crawl_gd/crawl_gd/spiders/gd_spider.py
@@ -1,25 +1,80 @@
 from scrapy import Request, Spider
 from urllib.parse import urlparse
 from pathlib import Path
+from crawl_gd.items import CrawlGdItem
 
 
 class DSSpider(Spider):
     name = "ds"
 
-    allowed_domains = ["glassdoor.com"]
+    # allowed_domains = ["glassdoor.com"]
+    # start_urls = [
+    #     "https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14.htm"
+    # ]
     start_urls = [
-        "https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14.htm"
+        r"file:///C:\Users\A2644752\Sandbox\Medium\DefineDS\crawl_gd\html"
+        r"\_Job_data-scientist-jobs-SRCH_KO0,14.htm"
     ]
 
     def parse(self, response, **kwargs):
-        # save html --> don't want to make multiple requests when building
-        url = response.url
-        parsed_url = urlparse(url=url)
-        url_path = parsed_url.path
-        file_name = url_path.replace("/", "_")
+        items = CrawlGdItem()
+        job_listings = response.css("li[data-test='jobListing']")
 
-        html_dir = Path.cwd() / "html"
-        html_dir.mkdir(parents=True, exist_ok=True)
+        for idx, job_listing in enumerate(job_listings, start=1):
+            id_ = job_listing.css("::attr(data-id)")
+            items["ID"] = id_.extract()
 
-        with open(html_dir / file_name, mode="wb") as file:
-            file.write(response.body)
+            adv_type = job_listing.css("::attr(data-adv-type)")
+            items["AdvType"] = adv_type.extract()
+
+            is_organic_job = job_listing.css("::attr(data-is-organic-job)")
+            items["IsOrganicJob"] = is_organic_job.extract()
+
+            ad_order_id = job_listing.css("::attr(data-ad-order-id)")
+            items["AdOrderID"] = ad_order_id.extract()
+
+            sgoc_id = job_listing.css("::attr(data-sgoc-id)")
+            items["SGOCID"] = sgoc_id.extract()
+
+            is_easy_apply = job_listing.css("::attr(data-is-easy-apply)")
+            items["IsEasyApply"] = is_easy_apply.extract()
+
+            title = job_listing.css("::attr(data-normalize-job-title)")
+            items["Title"] = title.extract()
+
+            loc = job_listing.css("::attr(data-job-loc)")
+            items["Location"] = loc.extract()
+
+            loc_id = job_listing.css("::attr(data-job-loc-id)")
+            items["LocationID"] = loc_id.extract()
+
+            loc_type = job_listing.css("::attr(data-job-loc-type)")
+            items["LocationType"] = loc_type.extract()
+
+            job_listing_a = job_listing.css("div > a")
+
+            # limit to first href with .get() instead of .extract()
+            href = job_listing_a.css("::attr(href)")
+            items["HREF"] = [href.get()]
+
+            # limit to first title with .get() instead of .extract()
+            company = job_listing_a.css("::attr(title)")
+            items["Company"] = [company.get()]
+
+            job_listing_img = job_listing_a.css("span > img")
+
+            logo_src = job_listing_img.css("::attr(src)")
+            items["LogoSRC"] = logo_src.extract()
+
+            logo_title = job_listing_img.css("::attr(title)")
+            items["LogoTitle"] = logo_title.extract()
+
+            job_listing_rating = job_listing.css(
+                "div > span[class='job-search-key-srfzj0 e1cjmv6j0']"
+            )
+
+            # # limit to first text with .get() instead of .extract()
+            # rating = job_listing_rating.xpath("//text()")
+            # items["CompanyRating"] = [rating.get()]
+
+            yield items
