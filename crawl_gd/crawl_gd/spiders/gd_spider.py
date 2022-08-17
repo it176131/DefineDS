04d77de34f02eb92c128cd65ba107869246a70a3
Ian Thompson <ianiat11@gmail.com>
2022-08-17 00:45:19 +0000

    Dev (#1)
    * modified:   crawl_gd/items.py
    	- removed comments
    	- added four new fields
    
    * modified:   crawl_gd/spiders/gd_spider.py
    	- added four new fields from items
    	- swapped start_url paths
    	- made get_next_page_url method to generate next page's url
    	- add ability to follow next page's url in .parse method
    
    * new file:   html/job_listings.json
    	- successfully collected 900 job listings
    
    Co-authored-by: Ian Thompson <ian.thompson@hrblock.com>

--- crawl_gd/crawl_gd/spiders/gd_spider.py
+++ crawl_gd/crawl_gd/spiders/gd_spider.py
@@ -7,16 +7,17 @@ from crawl_gd.items import CrawlGdItem
 class DSSpider(Spider):
     name = "ds"
 
-    # allowed_domains = ["glassdoor.com"]
-    # start_urls = [
-    #     "https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14.htm"
-    # ]
+    allowed_domains = ["glassdoor.com"]
     start_urls = [
-        r"file:///C:\Users\A2644752\Sandbox\Medium\DefineDS\crawl_gd\html"
-        r"\_Job_data-scientist-jobs-SRCH_KO0,14.htm"
+        "https://www.glassdoor.com/Job/data-scientist-jobs-SRCH_KO0,14.htm"
     ]
+    # start_urls = [
+    #     r"file:///C:\Users\A2644752\Sandbox\Medium\DefineDS\crawl_gd\html"
+    #     r"\_Job_data-scientist-jobs-SRCH_KO0,14.htm"
+    # ]
 
     def parse(self, response, **kwargs):
+        """"""
         items = CrawlGdItem()
         job_listings = response.css("li[data-test='jobListing']")
 
@@ -75,4 +76,54 @@ class DSSpider(Spider):
             )
             items["CompanyRating"] = [rating.get()]
 
+            urgency = job_listing.css("div[data-test='urgency-label']::text")
+            items["Urgency"] = urgency.extract()
+
+            age = job_listing.css("div[data-test='job-age']::text")
+            items["Age"] = age.extract()
+
+            salary_est = job_listing.css(
+                "span[data-test='detailSalary']::text"
+            )
+            items["SalaryEstimate"] = salary_est.extract()
+
+            salary_est_type = job_listing.css(
+                "span[data-test='detailSalary'] > span::text"
+            )
+            items["SalaryEstimateType"] = salary_est_type.extract()
+
             yield items
+
+        print("", "Did we make it?", "", sep="\n")
+        next_page_url = self.get_next_page_url(response)
+
+        if next_page_url is not None:
+            yield response.follow(next_page_url, callback=self.parse)
+
+    def get_next_page_url(self, response):
+        """Find and navigate to next page if available"""
+        print("", "Yes we did!", "", sep="\n")
+        # what page are we on
+        current_page_str = response.css(
+            "button[class='page selected css-1hq9k8 e13qs2071']"
+            "::attr(data-test)"
+        ).get()
+        current_page_list = current_page_str.split("-")
+        current_page_number = int(current_page_list[-1])
+
+        # determine max page count
+        page_range_text = response.css(
+            "div[class='paginationFooter']::text"
+        ).get()
+        page_range = page_range_text.split()
+        max_page_str = page_range[-1]
+        max_page_number = int(max_page_str)
+
+        # can we go further?
+        if current_page_number < max_page_number:
+            next_page_number = current_page_number + 1
+            next_page_url = f"https://www.glassdoor.com/Job/data-scientist" \
+                            f"-jobs-SRCH_KO0,14_IP{next_page_number}.htm"
+            return next_page_url
+
+        return None
